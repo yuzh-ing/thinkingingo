be5ff1185cce0ddbfccef5c65879df752516a627
yuzh-ing <1584558751@qq.com>
2024-03-21 23:55:29 +0800

    etree

--- examples/toolkit/file/xml/etree_test.go
+++ examples/toolkit/file/xml/etree_test.go
@@ -19,16 +19,17 @@ func TestPrintDuplicatePomDependency(t *testing.T) {
 	propMp := make(map[string][]*PropRowNo)
 	properties := doc.FindElement("./project/properties")
 	for i, v := range properties.ChildElements() {
-		propWithRowNo := new(PropRowNo)
+		propRowNo := new(PropRowNo)
 		arr := propMp[v.Tag]
 		if arr == nil {
 			arr = make([]*PropRowNo, 0)
 		}
-		arr = append(arr, propWithRowNo)
+		arr = append(arr, propRowNo)
 		propMp[v.Tag] = arr
 
-		propWithRowNo.Row = i
-		propWithRowNo.Value = v.Text()
+		propRowNo.Row = i
+		propRowNo.Value = v.Text()
+		propRowNo.PlainText = fmt.Sprintf("<%s>%s<%s>", v.Tag, v.Text(), v.Tag)
 	}
 
 	dependencyDefMp := make(map[string]*DependencyDef)
@@ -36,42 +37,64 @@ func TestPrintDuplicatePomDependency(t *testing.T) {
 	for row, v := range dependencies {
 		groupId := v.FindElement("groupId").Text()
 		artifactId := v.FindElement("artifactId").Text()
+		versionPlaceHolder := v.FindElement("version").Text()
 
 		mavenId := groupId + "::" + artifactId
 		def := dependencyDefMp[mavenId]
 		if def == nil {
 			def = NewDependencyDef()
+			def.groupId = groupId
+			def.artifactId = artifactId
 			dependencyDefMp[mavenId] = def
 		}
-		def.rows = append(def.rows, row)
+		def.dependencyRowNos = append(def.dependencyRowNos, &DependencyRowNo{
+			row,
+			groupId,
+			artifactId,
+			versionPlaceHolder,
+		})
 
-		versionPlaceHolder := v.FindElement("version").Text()
 		if !strings.Contains(versionPlaceHolder, "${") {
 			fmt.Println(mavenId + " 版本" + versionPlaceHolder + "不是占位符，忽略")
 			continue
 		}
-		versionPlaceHolder = versionPlaceHolder[2 : len(versionPlaceHolder)-1]
-		propRowNos := propMp[versionPlaceHolder]
+		version := versionPlaceHolder[2 : len(versionPlaceHolder)-1]
+		propRowNos := propMp[version]
 		if len(propRowNos) > 0 {
-			def.propRows = append(def.propRows, propRowNos...)
+			for _, propRowNo := range propRowNos {
+				flag := true
+				for _, defPropRow := range def.propRows {
+					if defPropRow.Row == propRowNo.Row {
+						flag = false
+						break
+					}
+				}
+				if flag {
+					def.propRows = append(def.propRows, propRowNo)
+				}
+			}
 		}
 	}
 
+	fmt.Println()
 	for k, v := range dependencyDefMp {
 		flag := false
-		if len(v.rows) > 1 {
-			fmt.Printf("%s 存在重复定义，序号：%v\n", k, v.rows)
+		if len(v.dependencyRowNos) > 1 {
+			fmt.Printf("%s 重复定义，明细如下：\n", k)
+			for _, dependencyRow := range v.dependencyRowNos {
+				fmt.Println("", dependencyRow.Text())
+			}
 			flag = true
 		}
 		if len(v.propRows) > 1 {
-			fmt.Printf("%s 存在多个版本定义，序号明细如下：\n", k)
+			fmt.Printf("%s 指定了多个版本，明细如下：\n", k)
 			for _, propRow := range v.propRows {
-				fmt.Printf("[%d]%s\n", propRow.Row, propRow.Value)
+				fmt.Println("  ", propRow.Text())
 			}
 			flag = true
 		}
 		if flag {
-			fmt.Println()
+			fmt.Println("================================================================================")
 		}
 	}
 }
@@ -82,17 +105,32 @@ type PropRowNo struct {
 	PlainText string
 }
 
-type DependencyDef struct {
+func (pr *PropRowNo) Text() string {
+	return pr.PlainText
+}
+
+type DependencyRowNo struct {
+	Row        int
 	groupId    string
 	artifactId string
-	rows       []int
-	propRows   []*PropRowNo
-	PlainText  string
+	version    string
+}
+
+func (dr *DependencyRowNo) Text() string {
+	fmtStr := "<dependency>\n\t<groupId>%s</groupId>\n\t<artifactId>%s</artifactId>\n\t<version>%s</version>\n</dependency>"
+	return fmt.Sprintf(fmtStr, dr.groupId, dr.artifactId, dr.version)
+}
+
+type DependencyDef struct {
+	groupId          string
+	artifactId       string
+	dependencyRowNos []*DependencyRowNo
+	propRows         []*PropRowNo
 }
 
 func NewDependencyDef() *DependencyDef {
 	result := new(DependencyDef)
-	result.rows = make([]int, 0)
+	result.dependencyRowNos = make([]*DependencyRowNo, 0)
 	result.propRows = make([]*PropRowNo, 0)
 	return result
 }
